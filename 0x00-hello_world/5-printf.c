4c6b4ef8749d014aee9c229cd8c09f9d3800447c
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-05 22:35:39 +0000

    fixed stale to betty guidelines

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char * line = "with proper grammar, but the outcome is a piece of art,\n";
+	char line = "with proper grammar, but the outcome is a piece of art,\n";
+
 	printf("%s", line);
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char * szof = "Size of ";
+	char szof[] = "Size of ";
 	int charsize = sizeof(char);
 	int intsize = sizeof(int);
 	int longintsize = sizeof(long int);
 	int longlongintsize = sizeof(long long int);
 	int floatsize = sizeof(float);
+
 	printf("%s a char: %d bytes(s)\n", szof, charsize);
 	printf("%s an int: %d bytes(s)\n", szof, intsize);
 	printf("%s a long int: %d bytes(s)\n", szof, longintsize);
 	printf("%s a long long int: %d bytes(s)\n", szof, longlongintsize);
 	printf("%s a float: %d bytes(s)\n", szof, floatsize);
 	printf("%s a char: %d bytes(s)\n", szof, charsize);
-	return(0);
+	return (0);
 }
