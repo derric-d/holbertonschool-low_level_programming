b8dea74fdd2a86de245885191bdf4d65c4b42c77
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-05 23:15:00 +0000

    recompile, rest global variable and repush

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char line = "with proper grammar, but the outcome is a piece of art,\n";
+	char line[] = "with proper grammar, but the outcome is a piece of art,\n";
 
 	printf("%s", line);
 	return (0);
