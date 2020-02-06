fd9c9ad340d587dabf513ec52bc6256f29060734
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-06 00:49:22 +0000

    switch compile option

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	puts("Size of a char: %d bytes(s)", sizeof(char));
-	puts("Size of an int: %d bytes(s)", sizeof(int));
-	puts("Size of a long int: %d bytes(s)", sizeof(long));
-	puts("Size of a long long int: %d bytes(s)", sizeof(long long));
-	puts("Size of a float: %d bytes(s)", sizeof(float));
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
 
 	return (0);
 }
