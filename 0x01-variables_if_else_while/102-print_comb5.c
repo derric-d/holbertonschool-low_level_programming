6e83f39e5ef53d10d3944d79372c578dd9368566
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-07 05:09:55 +0000

    add last adv task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	int tens, ones;
+	int flag = 1;
+
+	for (tens = 0; tens <= 99; tens++)
+	{
+		for (ones = 0; ones <= 99; ones++)
+		{
+			if (tens != ones && ones > tens)
+			{
+				if (!flag)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				else
+				{
+					flag = 0;
+				}
+				putchar('0' + tens / 10);
+				putchar('0' + tens % 10);
+				putchar(' ');
+				putchar('0' + ones / 10);
+				putchar('0' + ones % 10);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
