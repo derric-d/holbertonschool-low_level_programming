bae2d5ba750c2c7a1c1a1e34b156bc1e8f807edb
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-11 15:34:14 +0000

    added main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,6 @@
 /*
  * print_alphabet - prints alphabet
  */
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
-
-
 void print_alphabet(void)
 {
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
