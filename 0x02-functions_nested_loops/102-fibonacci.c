3d0b3785b41910e0804fd6604a6f9038d4b5779e
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-11 21:23:08 +0000

    fib seq

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: returns 1st 50 fib seqs
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int i, n, t1 = 1, t2 = 2, nextTerm;
+
+	n = 48;
+
+	printf("%lu, %lu", t1, t2);
+	for (i = 0; i <= n; i++)
+	{	
+		nextTerm = t1 + t2;
+		printf(", %lu", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+	}
+	printf("\n");
+	return (0);
+}
