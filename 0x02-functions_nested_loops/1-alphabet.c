5d12e5173e209459ee65268bc4121e3a84854995
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-11 15:30:12 +0000

    change to 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,10 @@ void print_alphabet(void)
 {
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 	int num;
-	int i = 0;
 
 	for (num = 0; num < 26; num++)
 	{
 		_putchar(alph[num]);
 	}
 	_putchar(10);
-	i++;
 }
