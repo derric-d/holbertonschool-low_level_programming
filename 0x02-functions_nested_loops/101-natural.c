db5be670f05e146459f956d7ca09f312e9b04fbf
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-11 21:05:01 +0000

    natural numbers problem

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int i, sum;
+
+	i = 0;
+	while (i <= 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;	
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
