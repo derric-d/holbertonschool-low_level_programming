d62b82c76ba4cd5c570e981c914a5df6d8988137
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-13 20:26:41 +0000

    fix header

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
@@ -14,5 +11,3 @@ void print_space(int);
 void print_square(int);
 void print_triangle(int);
 void fizzbuzz(void);
-
-#endif
