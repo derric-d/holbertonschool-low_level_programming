6c3b14d50bbff18b3e14efc69261893b15afe09c
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-13 21:39:23 +0000

    fix number print 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,10 +9,14 @@
 void print_line(int n)
 {
 	int i;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	if (n > 0)
 	{
-		_putchar(95);
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
