a679b2140eb42dafb1fc8e4e0afa1166b20d7dbd
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-14 04:16:44 +0000

    took out loop

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,12 @@
  */
 void print_number(int n)
 {
-	int flag = 1;
-	while (flag)
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			n = -n;
-		}
-		if (n / 10)
-			print_number(n / 10);
-		_putchar(n % 10 + '0');
-		flag = 0;
+		_putchar('-');
+		n = -n;
 	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
