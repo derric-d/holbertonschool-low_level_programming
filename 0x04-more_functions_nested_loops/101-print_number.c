1c6a215844969cc4c9709f8b5304cd799d05b1e7
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-14 04:14:54 +0000

    add loop

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,17 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	int flag = 1;
+	while (flag)
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		if (n / 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+		flag = 0;
 	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar((n % 10) + '0');
 }
