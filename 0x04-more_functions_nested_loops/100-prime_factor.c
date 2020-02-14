71da326286c4adf2171d48d63d15f4940bf43de2
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-14 01:13:18 +0000

    final solution for prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 /**
  * main - entry point
+ * @n: passed value
  * Description: find largest prime factor
- * Return: Always 0 (Success) 612852475143
+ * Return: Always 0 (Success)
  */
-long int getLargestPrimeFac(long int n); 
+long int getLargestPrimeFac(long int n);
 int main(void)
-{	
-	long int n = 38;
+{
+	long int n = 612852475143;
+
 	printf("%ld\n", getLargestPrimeFac(n));
 	return (0);
-
 }
 /**
- * largestFactors - finds largest prime factor of n
+ * getLargestPrimeFac - finds largest prime factor of n
  * @n: number being factored
  * Description: Finds largest prime Factor
  * Return: largest prime factor
  */
-long int getLargestPrimeFac(long int n) 
-{  
+long int getLargestPrimeFac(long int n)
+{
 	long int i;
 
 	for (i = 2; i < (n / 2); i++)
 	{
-    		if (n % i == 0)
+		if (n % i == 0)
 		{
 			n /= i;
 			i--;
 		}
 	}
 	return (n);
-} 
+}
