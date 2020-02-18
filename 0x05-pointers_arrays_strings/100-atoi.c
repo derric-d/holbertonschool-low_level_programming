815090b4f984d821e2feece3951b271e2aeb2a66
derric-d <derric.donehoo@gmail.com>
2020-02-18 22:50:45 +0000

    add check for negative and NULL

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+#define Is_NUMERIC_STRING(d) (*(char*)d >= 48) && (*(char*)d<= 57)
 
 int _atoi(char *s)
 {
+	/*
 	int result = 0;
 	while ((*s >= '0') && (*s <= '9'))
 	{
@@ -11,4 +13,24 @@ int _atoi(char *s)
 		s++;
 	}
 	return (result);
+	*/
+	int result = 0;
+	int sign = 1;
+	if (*s == ' ')
+		++s;
+	if (s == NULL)
+		return (0);
+	if ((*s) == '-')
+	{
+		sign = -1;
+
+		++s;
+	}
+
+	while (Is_NUMERIC_STRING(s))
+	{
+		result = (result * 10) + (*s - '0');
+                s++;
+	}
+	return (sign * result);
 }
