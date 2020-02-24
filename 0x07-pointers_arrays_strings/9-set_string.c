e8e1180630302b4fdb2580db4794c824e3d1eafa
Derric Donehoo <derric.donehoo@gmail.com>
2020-02-24 21:54:50 +0000

    add doc to 9

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+ * set_strings - set char to pointer
+ * @s: double pointer to string
+ * @to: pointer
+ */
 void set_string(char **s, char * to)
 {
 	*s = to;
