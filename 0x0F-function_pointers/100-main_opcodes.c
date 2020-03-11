ce295026511fb2f728d487269e935e35da0f815f
Derric Donehoo <derric.donehoo@gmail.com>
2020-03-11 19:53:37 +0000

    overlooked the test for negative bytes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,17 +16,22 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(2);
+		exit(1);
 	}
 	bytes = atoi(argv[1]);
-
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	while (bytes--)
 	{
 		/*
 		*0 - left pad w/zero 2 - minimum 2 ints in result
 		*h - interpret as short int x - unsigned hex
 		*/
-		printf("%02hhx%s", *p++, bytes ? " " : "\n");
+		printf("%02hhx", *p++);
+		printf("%s", bytes ? " " : "\n");
 	}
 	return (0);
 }
