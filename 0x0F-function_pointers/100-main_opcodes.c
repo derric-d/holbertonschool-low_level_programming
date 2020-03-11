c8215f1d94bb578509ffcaf2835438f1460d04fe
Derric Donehoo <derric.donehoo@gmail.com>
2020-03-11 19:43:26 +0000

    add adv proj to github

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+
+/**
+ * main - entry point
+ * @argc: num args
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int bytes;
+	char *p;
+
+	p = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	bytes = atoi(argv[1]);
+
+	while (bytes--)
+	{
+		/*
+		*0 - left pad w/zero 2 - minimum 2 ints in result
+		*h - interpret as short int x - unsigned hex
+		*/
+		printf("%02hhx", *p++);
+		if (bytes)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
