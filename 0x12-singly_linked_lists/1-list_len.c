c862adb1ff95cc55d0eb8391c851c37b2c0c20d1
derric-d <derric.donehoo@gmail.com>
2020-03-19 20:27:18 +0000

    first fix of errors

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,12 +7,14 @@
  */
 size_t list_len(const list_t *h)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	while (h != NULL)
+	const list_t *current = h;
+
+	while (current != NULL)
 	{
-		h = h->next;
 		i++;
+		current = current->next;
 	}
 	return (i);
 }
