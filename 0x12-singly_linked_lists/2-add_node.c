fea8792f5a216fb5341a0be5e05d1a8d87e1b74e
derric-d <derric.donehoo@gmail.com>
2020-03-19 20:13:13 +0000

    betty fixes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,6 +8,7 @@
 unsigned int _strlen(const char *s)
 {
 	unsigned int i = 0;
+
 	while (s[i])
 		i++;
 
@@ -15,12 +16,14 @@ unsigned int _strlen(const char *s)
 }
 /**
  * add_node - adds node to head of linked list
+ * @head: pointer to the pointer that starts the linked list
  * @str: string value for str member
- * @Return: the address of the new element
+ * Return: the address of the new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
