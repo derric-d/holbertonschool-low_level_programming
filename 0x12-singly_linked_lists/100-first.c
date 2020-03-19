f4eac9a16df265648a523f6c10b5fedc61dd089b
derric-d <derric.donehoo@gmail.com>
2020-03-19 16:00:19 +0000

    add adv proj to repo

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+void fast_print(void) __attribute__ ((constructor));
+/**
+ * fast_print - prints message before main is executed
+ */
+void fast_print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
