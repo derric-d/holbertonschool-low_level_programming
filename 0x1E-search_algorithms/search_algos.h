2802796a5478e05b49754cffcc74a16b1850b956
derric-d <derric.donehoo@gmail.com>
2020-08-26 20:25:18 +0000

    add first bit of search algos

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef __SEARCH_ALGORITHMS__
+#define __SEARCH_ALGORITHMS__
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+#endif
